dad1d136b864f5cd705f696a2898f5bae7bd133b
Szabó István <pisti4395@gmail.com>
2025-11-14 10:49:46 +0100

    refactort

--- calculator_test/testcalc.py
+++ calculator_test/testcalc.py
@@ -3,8 +3,10 @@ from calc import *
 
 def test_sum():
     assert sum(3,5) == 8
+def test_div():
     assert div(15,5) == 3
+def test_mul():
     assert mul(3,5) == 15
+def test_sub():
     assert sub(5,3) == 2
-    print("helyes")
     
